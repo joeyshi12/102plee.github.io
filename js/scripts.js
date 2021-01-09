639dc95cc4f6dcec18807944d9c582996ad251d1
itsananya <16752627+102plee@users.noreply.github.com>
2021-01-09 04:23:27 -0800

    Added Message Box Template
    Added the template for a message box that follows your bottom right corner
    
    Has no functionality at current time.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,14 @@ function createText() {
     text.innerText = "Joey Sucks.";
     document.getElementById("mainContent").appendChild(text);
 }
+
+document.addEventListener('click', function(event) {
+    if (!event.target.matches('.messageBubble')) return;
+    event.preventDefault();
+    var bubble = document.getElementById("message-Bubble");
+    var box = document.getElementById("message-Box");
+    box.style.height = "600px";
+    box.style.width = "400px";
+    bubble.style.display = "none";
+  
+  }, false);
