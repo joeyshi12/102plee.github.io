bda9343a9fdb53c5026866cce86e24c5e464b93f
itsananya <16752627+102plee@users.noreply.github.com>
2021-01-09 20:44:53 -0800

    Update scripts.js
    Made it so that the message bubble can retract, and uses jquery for most of it's functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,52 @@ function createText() {
     document.getElementById("mainContent").appendChild(text);
 }
 
+var blobopen = false;
 
 $(document).ready(function(){
 
-    $("#message-Bubble").on("click", function() {
-        console.log("sup");
-        var box = document.getElementById("message-Box");
-        var bubble = document.getElementById("message-Bubble");
-        bubble.style.display = "none";
-        box.style.height = "600px";
-        box.style.width = "400px";
-        $(this.hash).toggleClass('active').focus();
+    $(document).on('click', function (e) {
+        if (blobopen) {
+            if ($(e.target).closest("#message-Box").length === 0 && $(e.target).closest("#message-Bubble").length === 0) {
+                var box = document.getElementById("message-Box");
+                var bubble = document.getElementById("message-Bubble");
+                box.style.height = "75px";
+                box.style.width = "75px";
+
+                setTimeout(
+                    function() {
+                        bubble.style.display = "block";
+                    }, 500);
+
+                console.log("no");
+                blobopen = false;
+            }
+        }
     });
 
-    
-    $(window).on('beforeunload', function() {
-        console.log("sup");
-        $(window).scrollTop(0);
+    $("#message-Bubble").on("click", function() {
+        if (!blobopen) {
+            console.log("sup");
+            var box = document.getElementById("message-Box");
+            var bubble = document.getElementById("message-Bubble");
+            bubble.style.display = "none";
+            box.style.height = "600px";
+            box.style.width = "400px";
+            blobopen = true;
+        }
     });
+
+
+
+    // $("#message-Box").on('focusout', function () {
+    //     console.log("sup dude");
+    //     var box = document.getElementById("message-Box");
+    //     var bubble = document.getElementById("message-Bubble");
+    //     box.style.height = "75px";
+    //     box.style.width = "75px";
+    //     bubble.style.display = "block";
+    //     $("#message-Box").removeClass('active');
+    // });
 
     $("#doNothing").on("click", function() {
         return false;
